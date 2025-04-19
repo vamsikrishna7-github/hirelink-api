123eb41ab4889c2f6d8e1bcc782c6feca0dfb515
vamsikrishna <100037815+vamsikrishna7-github@users.noreply.github.com>
2025-04-19 15:01:28 +0530

    Email backend fixed

--- accounts/utils.py
+++ accounts/utils.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def send_password_reset_email(user, uid, token):
-    reset_link = f"http://localhost:3000/reset-password/{uid}/{token}/"  # Replace with your frontend reset URL
+    reset_link = f"https://hirelink-eta.vercel.app/reset-password/{uid}/{token}/"  
     subject = "Reset Your Password"
     message = f"""
     Hi {user.name or user.email},
